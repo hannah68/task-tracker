5fead3fadca853b538fb8d2ede8887d15ddb3d17
hannah68 <reihaneh.naderi1989@gmail.com>
2022-05-24 11:47:23 +0100

    add json server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
-
+import { TaskService } from 'src/app/services/task.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+ 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      return this.tasks = tasks
+    });
+  }
+
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(() => {
+      return this.tasks = this.tasks.filter(t => t.id !== task.id);
+    });
   }
 
 }
